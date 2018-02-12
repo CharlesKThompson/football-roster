4ee1bcd6c517ca10686d1086ca236b0203da2826
Charles <chuckness32@hotmail.com>
2018-02-11 21:34:46 -0700

    fourth commit

--- player-controller.js
+++ player-controller.js
@@ -31,12 +31,13 @@ PlayerController = function PlayerController() {
             var player = arr[i]
             //insert the "no info" options here
             template += `  
-             <h4>Player roster selection</h4>
-            <h4><b>Name:</b> ${player.firstname}</h4>
+            <div class="player-card">
+             <h5>Player selection</h5>
+            <h5><b>Name:</b> ${player.fullname}</h5>
             <P><b>Team:</b> ${player.pro_team}</P>
             <p><b>Position:</b> ${player.position}</p>
             <button class = "btn btn-primary" onclick="app.controllers.playerCtrl.addToTeam(${player.position})">Draft Player</button>
-
+            </div>
             `
         }
 
@@ -46,6 +47,22 @@ PlayerController = function PlayerController() {
         }
 
         playerRosterElem.innerHTML = template
+    }
+
+    function drawMyTeam(arr) {
+        var template = ''
+        for (var i = 0; i < arr.length; i++) {
+            var player = arr[i]
+
+            template += `
+            <h4>My Team roster selection</h4>
+            <h4><b>Name:</b> ${player.fullname}</h4>
+            <P><b>Team:</b> ${player.pro_team}</P>
+            <p><b>Position:</b> ${player.position}</p>
+            <button class = "btn btn-primary" onclick="app.controllers.playerCtrl.removeFromTeam(${player.position})">Fire Player</button>
+            `
+        }
+        myTeamElem.innerHTML = template
     }
 
     function drawMyTeam(arr) {
